ac03eb20f83b049d24a06b1ac9005c2a3afa2ffd
TimKral5 <TimKral5@outlook.com>
2022-01-19 12:53:35 +0100

    Projektdateien hinzufügen.

---
+++ QrCodeGenerator/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using QRCoder;
+using Newtonsoft.Json;
+using static QRCoder.PayloadGenerator;
+using System.IO;
+
+namespace QrCodeGenerator
+{
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private byte[] png { get; set; }
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void EncodeBtn_Click(object sender, RoutedEventArgs e)
+		{
+			bool isText = false;
+
+			Payload payload = null;
+			if ((bool)PayLoadTypeUrlRad.IsChecked)
+				payload = new Url(DataTb.Text);
+
+			else if ((bool)PayLoadTypeSmsRad.IsChecked)
+			{
+				Dictionary<string, string> smsArgs =
+					JsonConvert.DeserializeObject<Dictionary<string, string>>(DataTb.Text);
+				payload = new SMS(smsArgs["num"], smsArgs["msg"]);
+			}
+
+			else if ((bool)PayLoadTypeWifiRad.IsChecked)
+			{
+				Dictionary<string, string> wifiArgs = 
+					JsonConvert.DeserializeObject<Dictionary<string, string>>(DataTb.Text);
+				WiFi.Authentication auth;
+
+				switch (wifiArgs["auth"])
+				{
+					case "wpa":
+						auth = WiFi.Authentication.WPA;
+						break;
+					case "wep":
+						auth = WiFi.Authentication.WEP;
+						break;
+					case "nopass":
+					default:
+						auth = WiFi.Authentication.nopass;
+						break;
+				}
+				payload = new WiFi(wifiArgs["ssid"], wifiArgs["pass"], auth);
+			}
+			else if ((bool)PayLoadTypeWathsappMessageRad.IsChecked)
+			{
+				Dictionary<string, string> whatsappArgs =
+					JsonConvert.DeserializeObject<Dictionary<string, string>>(DataTb.Text);
+				payload = new WhatsAppMessage(whatsappArgs["num"], whatsappArgs["msg"]);
+			}
+			else
+			{
+				isText = true;
+			}
+
+			QRCodeGenerator generator = new QRCodeGenerator();
+			QRCodeData data;
+			if (!isText)
+				data = generator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+			else 
+				data = generator.CreateQrCode(DataTb.Text, QRCodeGenerator.ECCLevel.Q);
+			PngByteQRCode pngQrCode = new PngByteQRCode(data);
+			png = pngQrCode.GetGraphic(20);
+		}
+
+		private void SaveBtn_Click(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog dialog = new SaveFileDialog();
+			if (dialog.ShowDialog() == true)
+				File.WriteAllBytes(dialog.FileName, png);
+		}
+	}
+}
---
+++ QrCodeGeneratorFileToQr/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+namespace QrCodeGeneratorFileToQr
+{
+	class Program
+	{
+		private static byte[] png { get; set; }
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(args[0]);
+			bool isText = false;
+			Payload payload = null;
+			string filePath = "";
+			string file = "";
+			string dataType = "";
+			if (args.Length != 1)
+			{
+				return;
+			}
+			else
+			{
+				filePath = args[0];
+				dataType = filePath.Split('.')[filePath.Split('.').Length - 1];
+				file = File.ReadAllText(filePath);
+			}
+
+			switch (dataType)
+			{
+				case "txt":
+					isText = true;
+					break;
+				case "sms":
+					break;
+				default:
+					break;
+			}
+
+			QRCodeGenerator generator = new QRCodeGenerator();
+			QRCodeData data;
+			if (!isText)
+				data = generator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+			else
+				data = generator.CreateQrCode(file, QRCodeGenerator.ECCLevel.Q);
+			PngByteQRCode pngQrCode = new PngByteQRCode(data);
+
+			File.WriteAllBytes($"{filePath}.png", pngQrCode.GetGraphic(20));
+		}
+	}
+}
